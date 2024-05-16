acbd9c33797cbee8fd083ec1a09f980603f42dee
NepoDunz <ndunz@worldbank.org>
2024-05-16 18:14:39 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -123,7 +123,7 @@ def make_heatmap(input_df, input_y, input_x, input_color, input_color_theme):
                              scale=alt.Scale(scheme=input_color_theme)),
             stroke=alt.value('black'),
             strokeWidth=alt.value(0.25),
-        ).properties(width=900
+        ).properties(width=500
         ).configure_axis(
         labelFontSize=12,
         titleFontSize=12
