ce0cdc732e0f8a428515bcfb5c35e731d0f3992f
NepoDunz <ndunz@worldbank.org>
2024-05-16 17:25:33 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -299,7 +299,7 @@ with st.expander('About', expanded=True):
 
 with col[0]:
     st.markdown('#### Top Risk Countries')
-    st.column_config.LineChartColumn("Trend", color="green")
+    
     st.dataframe(df_selected_year_sorted,
                  column_order=("Countries", "Overall risk factor", "Fiscal risk factor", "Financial risk factor", "External risk factor"),
                  hide_index=True,
