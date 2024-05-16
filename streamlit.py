4ada9b4cac51c6d1b707185d5a278df14d8e7582
NepoDunz <ndunz@worldbank.org>
2024-05-16 14:53:15 -0400

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -215,7 +215,7 @@ with col[1]:
 #     st.plotly_chart(choropleth, use_container_width=True)
 
     world_chart = make_world_chart(df_selected_year, 'country_code', 'risk factor', selected_color_theme)
-    st.pyplot(world_chart, use_container_width=True)
+    st.altair_chart(world_chart, use_container_width=True)
     
     heatmap = make_heatmap(df_reshaped, 'year', 'country_code', 'risk factor', selected_color_theme)
     st.altair_chart(heatmap, use_container_width=True)
